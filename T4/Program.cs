5eab05cfb57d0104b1ad5e6136848730dc0f864c
IlyaGardin <113240912+IlyaGardin@users.noreply.github.com>
2022-10-23 20:23:30 +0400

    Add files via upload

---
+++ T4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите колличество треугольников: ");
+            int l;
+            if (int.TryParse(Console.ReadLine(), out l) && l>0 && l < int.MaxValue)
+            {
+
+                for (int n = 1; n <= l; n++)
+                {
+                    int k = 1;
+                    for (int i = 1; i <= n; i++)
+                    {
+                        for (int j = 0; j < (n - i) + (l - n); j++)
+                        {
+                            Console.Write(" ");
+                        }
+                        for (int j = 0; j < k; j++)
+                        {
+                            Console.Write("*");
+                        }
+                        k += 2;
+                        Console.WriteLine();
+                    }
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Некорректно введено колличество треугольников.");
+            }
+        }
+    }
+}
