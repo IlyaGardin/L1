2457ce4276cd4c8204943091749b3ab18480235f
IlyaGardin <113240912+IlyaGardin@users.noreply.github.com>
2022-10-23 20:23:57 +0400

    Add files via upload

---
+++ T5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int summa = 0;
+            for (int i = 1; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    summa += i;
+                }
+            }
+            Console.WriteLine(summa);
+        }
+    }
+}
